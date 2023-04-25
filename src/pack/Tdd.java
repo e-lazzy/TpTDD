421e6a767bfe62818e4bb08d39d127eaab07823b
e-lazzy <43212505+e-lazzy@users.noreply.github.com>
2023-04-25 14:49:55 +0200

    Exi3

--- src/pack/Tdd.java
+++ src/pack/Tdd.java
@@ -6,16 +6,15 @@ public class Tdd {
 		
 	}
 	
-	public String exi1(String nom) {
-		
-		return "Hello, " + nom.substring(0, 1).toUpperCase() + nom.substring(1)+ ".";
-	}
-	
-public String exi2(String nom) {
+public String welcome(String nom) {
 		if(nom==null || nom.isEmpty() || nom.isBlank() || "".equals(nom)) 
 			return "Hello, my friend.";
+		else if(nom.toUpperCase().equals(nom)) {
+			return "HELLO, "+ nom + " !";
+		}
+		
 		else
 			return "Hello, " + nom.substring(0, 1).toUpperCase() + nom.substring(1)+ ".";
 	}
-}
 
+}
--- test/pack/TddTest.java
+++ test/pack/TddTest.java
@@ -1,5 +1,6 @@
 package pack;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -15,39 +16,54 @@ class TddTest {
 
 	@Test
 	public void testeExi1_1() {
-		assertEquals("Hello, Bob.", tdd.exi1("Bob"));
+		assertEquals("Hello, Bob.", tdd.welcome("Bob"));
 	}
 	@Test
 	public void testeExi1_2() {
-		assertEquals("Hello, Bob.", tdd.exi1("bob"));
+		assertEquals("Hello, Bob.", tdd.welcome("bob"));
 	}
 	@Test
 	public void testeExi1_3() {
-		assertEquals("Hello,  .", tdd.exi1(" "));
+		assertNotEquals("Hello, kiki.", tdd.welcome("Kiki"));
 	}
 	
 	@Test
 	public void testeExi1_4() {
-		assertEquals("Hello, Kevin.", tdd.exi1("kevin"));
+		assertEquals("Hello, Kevin.", tdd.welcome("kevin"));
 	}
 	
 	@Test
 	public void testeExi2_1() {
-		assertEquals("Hello, my friend.", tdd.exi2(""));
+		assertEquals("Hello, my friend.", tdd.welcome(""));
 	}
 	
 	@Test
 	public void testeExi2_2() {
-		assertEquals("Hello, my friend.", tdd.exi2(null));
+		assertEquals("Hello, my friend.", tdd.welcome(null));
 	}
 	
 	@Test
 	public void testeExi2_3() {
-		assertEquals("Hello, my friend.", tdd.exi2("  "));
+		assertEquals("Hello, my friend.", tdd.welcome("  "));
 	}
 	
 	@Test
-	public void testeExi1_5() {
-		assertEquals("Hello, Bob.", tdd.exi1("Bob"));
+	public void testeExi3_1() {
+		assertEquals("HELLO, BELETTE !", tdd.welcome("BELETTE"));
+	}
+	
+	@Test
+	public void testeExi3_2() {
+		assertEquals("HELLO, FRANCOIS !", tdd.welcome("FRANCOIS"));
+	}
+	
+	@Test
+	public void testeExi3_3() {
+		assertEquals("Hello, BERNAD.", tdd.welcome("bERNAD"));
+	}
+	
+	@Test
+	public void testeExi3_4() {
+		assertEquals("Hello, ZaCk.", tdd.welcome("ZaCk"));
 	}
 }
